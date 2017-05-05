15a5d9f1368a1e9e292f1959eb100a4152b9eb7b
tdunn1248 <rover@Thomass-MacBook-Pro.local>
2017-05-04 21:23:56 -0700

    Clean up

--- app.js
+++ app.js
@@ -1,49 +1,36 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const app = express()
 const router = express.Router()
 const http = require('http')
 const request = require('request')
-const beauty = require('pug-beautify')
-// const path = require('path')
 
+// Instantiate app
+const app = express()
 
+// const path = require('path') ?
+app.set('view engine', 'pug')
+
+//configure middleware
 app.use(express.static(__dirname + '/public'))
-app.set('view engine', beauty('pug'))
 app.use(bodyParser())
-//app.use(bodyParser().JSON())
 
 // initial render of you home page
 app.get('/', function(req, res) {
   res.render('index', { message: 'Hello' })
+})
+// wassup world
+app.get('/send', function(req, res) {
+  res.send('hello world')
 })
 
 // endpoint for buidling a HTTP request
 app.post( '/request/build', function( req, res ) {
   let bod;
   const url = req.body.host
-  request(url , (error, response, body) => {
-    bod = body
-  })
-  res.render('index', { reqBody: JSON.stringify(req.body), resBody: bod })
+  res.send({ reqBody: JSON.stringify(req.body), reqBody: reqBody })
   res.redirect('/')
 })
 
-//how to connect html or how to ge the host and post into request?
-//how to post all info into text area?
-
-//post to database
-//respond with the head and the body?
-app.post('/send', function(req, res) {
-  res.send('hello world')
-
-  // const host = req.body.host
-  // const method = req.body.method
-  // request(host).pipe(res.send)
-  // res.send(req.body)
-  // console.log(req.body)
-  // res.redirect('/')
-})
 
 //http.createServer()
 app.listen(3001, function() {
--- public/main.js
+++ public/main.js
@@ -1,30 +1,8 @@
-
-
 function grabInputs() {
   let method = document.querySelector('#methodInput').value
   let host = document.querySelector('#hostInput').value
-  // let requestBody = (method, host) => {
-  //     method = method
-  //     host = host
-  //     return [method, host]
-  //    }
-  // return JSON.stringify(requestBody(method, host))
-  //
-  // let inputs = {method, host}
-  // return inputs
-  // GET /search?q=hedgehog HTTP/1.1
-  // Host: www.google.com
-  // User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36
-  // Accept: text/html
-
-  // fetch('host').then(function(name){
-  //   return res.hostname = name
-  // })
-  // method.value =
-  //     host.value =
-
   let inputs = [method + ' /' + '\n' + 'HOST: ' + host + '\n']
-  return inputs
+    return inputs
 }
 
 grabInputs()
@@ -37,7 +15,6 @@ function buildRequest() {
   // console.log(theRequest)
   return receive.value = grabInputs();//server info
 }
-
 
 function buildSend() {
   let response = document.querySelector('.resBody')
